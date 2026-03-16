c53c6ecdc5fff43595bd68c2bd723a21edd70238
nisaavara37 <nisaavara37@gmail.com>
2026-03-16 13:39:10 +0300

    Add location, housing preferences, and user models

--- core/models.py
+++ core/models.py
@@ -15,6 +15,7 @@ class Profile(models.Model):
     bio = models.TextField(blank=True, null=True)
     mbti_type = models.ForeignKey(MBTIType, on_delete=models.SET_NULL, null=True)
     budget_limit = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
         return self.user.username
@@ -45,3 +46,62 @@ class Match(models.Model):
 
     def __str__(self):
         return f"{self.user_1.username} & {self.user_2.username} - Skor: {self.algorithm_score}"
+
+#6. Ev tercihleri
+class HousingPreference(models.Model):
+
+    ROOM_TYPE_CHOICES = [
+        ('private', 'Private Room'),
+        ('shared', 'Shared Room')
+    ]
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=10, choices=ROOM_TYPE_CHOICES)
+    max_roommates = models.IntegerField()
+    has_pet = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.profile.user.username} Housing Preference"
+
+#7. Profil için fotoğraf kısmı
+class UserPhoto(models.Model):
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="profile_photos/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.profile.user.username} Photo"
+
+#8. Kullanıcıların birbirlerini beğenmesi:
+class Like(models.Model):
+    from_user = models.ForeignKey(User, related_name="likes_sent", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name="likes_received", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_user.username} likes {self.to_user.username}"
+
+#9. Mesajlaşma sistemi
+class Message(models.Model):
+
+    sender = models.ForeignKey(User, related_name="sent_messages", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name="received_messages", on_delete=models.CASCADE)
+    content = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.username} -> {self.receiver.username}"
+
+#10. Compability Score Tablosu(daha çok şey eklenebilir)
+class CompatibilityScore(models.Model):
+
+    user1 = models.ForeignKey(User, related_name="compatibility_user1", on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, related_name="compatibility_user2", on_delete=models.CASCADE)
+    mbti_score = models.FloatField()
+    lifestyle_score = models.FloatField()
+    interest_score = models.FloatField()
+    total_score = models.FloatField()
+    calculated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user1.username} & {self.user2.username} -> {self.total_score}"
