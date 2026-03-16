8ae8b6bdfe9ee99d5c637383b06f065ffe2d6cf1
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-03-16 13:28:56 +0300

    Update URL patterns for API endpoints

--- typematch/urls.py
+++ typematch/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Core uygulamasının URL rotalarını ana sisteme (Root) bağlıyoruz
-    path('', include('core.urls')), 
+    path('api/match/<int:user1_id>/<int:user2_id>/', views.calculate_match_api),
+    
+    # Kimlik Doğrulama Uç Noktaları
+    path('api/register/', views.register_api),
+    path('api/login/', views.login_api),
+    path('api/logout/', views.logout_api),
 ]
