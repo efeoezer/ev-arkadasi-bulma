444d656dc58a5d4b1d1b44feeb9cf64e7a4da289
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-03-19 19:00:34 +0300

    Modify password reset view template
    Updated the password reset view to use a custom template.

--- typematch/urls.py
+++ typematch/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # Gelen tüm istekleri doğrudan core uygulamasının urls.py dosyasına yönlendirir
     path('', include('core.urls')), 
     # Şifre Sıfırlama Yolları
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='core/forgot_password.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
