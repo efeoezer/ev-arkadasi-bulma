ac7a58905387bfc268de227186ff71f9e65bb4b8
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-03-24 22:17:17 +0300

    Fix registration of Match model in admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import MBTIType, Profile, LifestyleTag, UserLifestyle, Match
+from .models import Profile, LifestyleTag, UserLifestyle, Match
 
 # Veritabanı tablolarının arayüze entegre edilmesi
-admin.site.register(MBTIType)
 admin.site.register(Profile)
 admin.site.register(LifestyleTag)
 admin.site.register(UserLifestyle)
