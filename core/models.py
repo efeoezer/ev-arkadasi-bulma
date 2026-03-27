e447ca1eccacf9dae507d30995bf2cb1ece776de
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-03-28 00:50:29 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,21 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# 1. KONUM TABLOSU
-class Location(models.Model):
-    city = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.city}, {self.country}"
-
 # 2. PROFİL TABLOSU
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True, help_text="Kullanıcının kendisini tanıttığı kısa biyografi.")
+    city = models.CharField(max_length=100, blank=True, null=True, help_text="Yaşadığı şehir")
     mbti_type = models.CharField(max_length=4, blank=True, null=True)
     budget_limit = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
     
     def __str__(self):
         return f"{self.user.username} - {self.mbti_type if self.mbti_type else 'Belirsiz'}"
