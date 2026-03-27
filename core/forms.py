71d0550b85651aaf73e0b6ba9fbc4a72e11e1722
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-03-28 00:28:22 +0300

    Create forms.py

---
+++ core/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
+
+# 1. Kullanıcı (User) tablosundaki temel bilgileri güncellemek için
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# 2. Profil (Profile) tablosundaki ekstra bilgileri güncellemek için
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'city'] # Kullanıcının kendisini tanıtacağı alanlar
