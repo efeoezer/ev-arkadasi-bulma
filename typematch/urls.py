6b00b690b9045f4a314daceefe5f3ffea59d27da
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-03-28 22:03:53 +0300

    Update urls.py

--- typematch/urls.py
+++ typematch/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,10 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('admin/', admin.site.register),
+    path('', include('core.urls')),
 ]
+
+# Geliştirme modundaysak medya dosyalarını sunmaya izin ver
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
