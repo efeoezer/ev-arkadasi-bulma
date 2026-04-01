ed06ddab27a888ec81d15d3aa3158177be1a6cf7
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-04-01 21:47:25 +0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from .models import Profile, RoommatePreference, Verification, Match, Like, Message, Review, UserPhoto
+from .models import RoommatePreference, Match, Like, Message, Review
 
-admin.site.register(Profile)
 admin.site.register(RoommatePreference)
-admin.site.register(Verification)
 admin.site.register(Match)
 admin.site.register(Like)
 admin.site.register(Message)
 admin.site.register(Review)
-admin.site.register(UserPhoto)
