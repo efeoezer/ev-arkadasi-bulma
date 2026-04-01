ce6d8fbfbf75dc8a49d459ae55013e3cbc169f14
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-04-01 21:47:59 +0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, UserPhoto, Verification
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(UserPhoto)
+admin.site.register(Verification)
