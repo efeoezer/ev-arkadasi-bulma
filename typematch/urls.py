459aa1e0d9db521f8add4341f7f8a9b367e620b6
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-04-10 19:50:02 +0300

    Update urls.py

--- typematch/urls.py
+++ typematch/urls.py
@@ -1,20 +1,23 @@
-from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')), 
+    
+    # Uygulama Yolları
+    path('', include('core.urls')),           # Ana sayfa ve eşleşme (core)
+    path('accounts/', include('accounts.urls')), # Giriş/Kayıt (accounts)
+    path('chat/', include('chat.urls')),         # Mesajlaşma (chat)
+
+    # Şifre Sıfırlama (Django Standart)
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='core/forgot_password.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('admin/', admin.site.register),
-    path('', include('core.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('chat/', include('chat.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
