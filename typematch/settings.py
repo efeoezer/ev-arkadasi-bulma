d4c4136a18d0338b3ee4cb398cb3901c0d045b3f
Efe Özer <75304261+efeoezer@users.noreply.github.com>
2026-04-12 11:46:43 +0300

    Update settings.py

--- typematch/settings.py
+++ typematch/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'typematch.middleware.ActiveUserMiddleware',
+    #'typematch.middleware.ActiveUserMiddleware',
 ]
 
 ROOT_URLCONF = 'typematch.urls'
