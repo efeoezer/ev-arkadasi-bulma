a904a233143d0b947e37b4104324f4c3fcdbefd7
nisaavara37 <nisaavara37@gmail.com>
2026-05-02 13:42:05 +0300

    Modify ProfileAdmin list display and filters
    Updated ProfileAdmin to include verification and online status fields.

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,9 @@ from .models import Profile, UserPhoto, Verification
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'last_seen', 'get_online_status')
+    list_display = ['user', 'is_verified', 'is_online'] # Admin panelinde yan yana gösterilecek alanlar
+    list_editable = ['is_verified']                    # Doğrudan listeden tik atıp kaydetmeyi sağlar
+    list_filter = ['is_verified']                      # Sağ tarafa filtreleme menüsü ekler
     
     def get_online_status(self, obj):
         return obj.is_online()
