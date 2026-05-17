55ea185922850989620cfa76dd981ec7f51a6b6a
Efe Özer <efeozer@Euclid.local>
2026-05-17 17:02:16 +0300

    feat: postgresql entegrasyonu yapildi, .env ile guvenlik saglandi

--- typematch/settings.py
+++ typematch/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,14 +84,13 @@ WSGI_APPLICATION = 'typematch.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'veritabanı_adınız',
-        'USER': 'kullanıcı_adınız',
-        'PASSWORD': 'şifreniz',
-        'HOST': 'localhost',  # Canlıda sunucu adresi
-        'PORT': '5432',       # PostgreSQL standart portu
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
